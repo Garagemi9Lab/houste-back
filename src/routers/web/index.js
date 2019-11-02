714ce30ca91146c2677dcab75ed6b0f046b3f645
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2019-11-02 19:20:01 -0300

    fixes

--- src/routers/web/index.js
+++ src/routers/web/index.js
@@ -29,7 +29,7 @@ const message = async (req, res) => {
     const params = req.body
     const place = req.params.place
     if (!params.context) params.context = {}
-    if (!params.context.LOCAL) params.context.LOCAL = place
+    if (!params.context.LOCAL) params.context.LOCAL = place.replace(new RegExp(/_/g), ' ')
     // if (!params.context.skills['main skill']) params.context.skills['main skill'] = {}
     // if (!params.context.skills['main skill'].user_defined) params.context.skills['main skill'].user_defined = {}
     // if (!params.context.skills['main skill'].user_defined.LOCAL) params.context.skills['main skill'].user_defined.LOCAL = place
